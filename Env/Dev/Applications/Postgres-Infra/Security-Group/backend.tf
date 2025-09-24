537d3385012d25df2af22b58b110cba734003ca2
sonalsinghroha <97205218+sonalsinghroha@users.noreply.github.com>
2025-09-24 23:26:12 +0530

    Update backend.tf

--- Env/Dev/Applications/Postgres-Infra/Security-Group/backend.tf
+++ Env/Dev/Applications/Postgres-Infra/Security-Group/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "otms-dev-cloud-ops-crew"
-    key     = "Applications/Postgres-Infra/Security-Group/terraform.tfstate"
+    key     = "Databases/Postgres-Infra/Security-Group/terraform.tfstate"
     region  = "us-east-2"
     encrypt = true
   }
